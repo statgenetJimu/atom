cc5a084d6e53978401a3252be0988138fe13660d
Ryo Yamada <ryamada@RyonoMacBook-Air1.local>
2019-02-22 11:45:41 +0900

    AdjacencyAlgebra

--- R/FockBasis.R
+++ R/FockBasis.R
@@ -1,1 +1,11 @@
 # グラフのフォック空間基底
+
+library(igraph)
+n <- 10
+A <- matrix(sample(0:1,n^2,replace=TRUE,prob=c(0.3,0.7)),n,n)
+A <- A + t(A)
+diag(A) <- 0
+A[which(A!=0)] <- 1
+g <- graph.adjacency(A)
+plot(g)
+image(A)
