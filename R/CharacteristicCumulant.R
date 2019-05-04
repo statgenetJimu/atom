91221ef6740ec74166b4ee2a257f899bb713c131
unknown <ryamada@genome.med.kyoto-u.ac.jp>
2019-05-04 18:22:57 +0900

    cyclePermutation

---
+++ R/CharacteristicCumulant.R
@@ -0,0 +1,12 @@
+# moment generating function
+# cumulant generating function
+
+## probability measure : "確率測度（かくりつそくど、英: probability measure）とは、標本空間に事象となる完全加法族が与えられたとき、事象の確率を測る測度のことである"
+## Wikipedia
+
+# poisson distributionの確率密度関数
+# P(k|lambda) = lambda^k/k! e^(-lambda)
+# 特性関数
+my.phi.pois <- function(t,lambda){
+
+}
